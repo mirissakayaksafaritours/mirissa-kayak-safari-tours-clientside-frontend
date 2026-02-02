cde31eb9c456f1aaa7fd1a9d48d983b202719c35
dinukaprab <dinukaprab@gmail.com>
2026-02-02 12:50:53 +0530

    hero section add to home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,22 @@
 import { Header } from "@/components/header/header";
 import { Footer } from "@/components/footer/footer";
+import { Hero } from "@/components/hero";
 
 export default function Home() {
   return (
     <>
       <Header />
-      <main className="min-h-screen bg-gradient-to-b from-background to-muted"></main>
+      <main>
+        <Hero />
+        {/* 
+          <FeaturedTours />
+          <WhyChooseUs />
+          <FAQAccordion />
+          <OperatingHours />
+          <HomeMapSection />
+          <Reviews /> 
+          */}
+      </main>
       <Footer />
     </>
   );
