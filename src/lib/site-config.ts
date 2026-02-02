e960c9713f25fe90d624e0acb12992e59d0b066b
dinukaprab <dinukaprab@gmail.com>
2026-02-02 13:35:38 +0530

    add googlemapsUrl to siteConfig file

--- src/lib/site-config.ts
+++ src/lib/site-config.ts
@@ -6,6 +6,8 @@ export const siteConfig = {
   email: "info@mirissakayak.com",
   whatsapp: "94771234567",
   address: "Mirissa Beach, Matara District, Sri Lanka",
+  googleMapsUrl:
+    "https://www.google.com/maps/search/?api=1&query=Mirissa+Beach+Sri+Lanka",
   mapEmbed:
     "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d15898.132132132!2d80.4524!3d5.9481!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3ae139c5c6b4f3c1%3A0x7e8e8e8e8e8e8e8e!2sMirissa%2C%20Sri%20Lanka!5e0!3m2!1sen!2slk!4v1234567890",
   social: {
